2c97e94a8789ebfa269f6a175d97c13436aace41
DanielS12345678 <21148ds@hvhs.school.nz>
2025-08-03 04:10:20 +0000

    V7 Completed close view pages (3/8) Milestone, with text, images and buttons for taking the user to the order form and taking the user back to thier last page have been added for each individual plant, other little chnages have been done to help tidy up code E.G. removing the hoveroverelement class tag

--- scripts/fbP_manager.js
+++ scripts/fbP_manager.js
@@ -7,18 +7,15 @@
 /*********************************************/
 function fbP_zoomOnPlant() {
 
-  const ZOOMASMALLIMAGE = document.getElementById('i_closeSmallSwanPlant');
-    //const ZOOMAMEDIUMIMAGE = document.getElementById('i_closeMediumSwanPlant');
-  //const ZOOMALARGEIMAGE = document.getElementById('i_closeLargeSwanPlant');
-  //const ZOOMAEXLARGEIMAGE = document.getElementById('i_closeExtraLargeSwanPlant');
-
+  const ZOOMIMAGE = document.getElementById('i_closeViewSwanPlant');
+    
     var currentZoom = 1;
     const ZOOMSPEED = 0.3; 
 
 
-  /***** Small Image Zoom *****/
+  /***** Image Zoom *****/
   // listening for the scroll wheel on the image 
-    ZOOMASMALLIMAGE.addEventListener('wheel', (zoom) => {
+    ZOOMIMAGE.addEventListener('wheel', (zoom) => {
    
    //     event.preventDefault();
 
@@ -35,10 +32,12 @@ function fbP_zoomOnPlant() {
 
         // Apply the new zoom level
 
-     ZOOMASMALLIMAGE.style.transform = `scale(${currentZoom})`;
+     ZOOMIMAGE.style.transform = `scale(${currentZoom})`;
     });
     
 }
+
+
 
 /*********************************************/
 // fbP_backToTop()
@@ -58,220 +57,3 @@ console.log("backToTop(): Start");
   console.log("backToTop(): Completed");
 }
 */
-const ISMALLSWANPLANT = document.getElementById('i_smallSwanPlant');
-const IMEDSWANPLANT = document.getElementById('i_mediumSwanPlant');
-const ILARGESWANPLANT = document.getElementById('i_largeSwanPlant');
-const IEXLARGESWANPLANT = document.getElementById('i_extraLargeSwanPlant');
-
-const DSMALLSWANPLANT = document.getElementById('d_smallSwanPlant');
-const DMEDSWANPLANT = document.getElementById('d_mediumSwanPlant');
-const DLARGESWANPLANT = document.getElementById('d_largeSwanPlant');
-const DEXLARGESWANPLANT = document.getElementById('d_extraLargeSwanPlant');
-
-
-
-
-function startone(){
-console.log("startone()")
-const DSMALLSWANPLANT = document.getElementById('d_smallSwanPlant')
-const DMEDSWANPLANT = document.getElementById('d_mediumSwanPlant')
-
-DSMALLSWANPLANT.addEventListener("mouseenter", fbp_viewEnterSmall());
-DSMALLSWANPLANT.addEventListener("mouseleave", fbp_viewExitSmall());
-
-
-DMEDSWANPLANT.addEventListener("mouseenter", fbp_viewEnterMed());
-DMEDSWANPLANT.addEventListener("mouseleave", fbp_viewExitMed());
-
-DLARGESWANPLANT.addEventListener("mouseenter", fbp_viewEnterLarge());
-DLARGESWANPLANT.addEventListener("mouseleave", fbp_viewExitLarge());
-
-DEXLARGESWANPLANT.addEventListener("mouseenter", fbp_viewEnterExlarge());
-DEXLARGESWANPLANT.addEventListener("mouseleave", fbp_viewExitExlarge());
-
-}
-
-
-function fbp_viewEnterSmall(){
-
-  const ISMALLSWANPLANT = document.getElementById('i_smallSwanPlant');
-const DSMALLSWANPLANT = document.getElementById('d_smallSwanPlant');
-
-const DMEDSWANPLANT = document.getElementById('d_mediumSwanPlant')
-
-const DLARGESWANPLANT = document.getElementById('d_largeSwanPlant');
-const DEXLARGESWANPLANT = document.getElementById('d_extraLargeSwanPlant');
-
-
-  const PLANTGRIDBOT = document.getElementById('d_viewPlantsGridBot')
-
-  // Small image
-  ISMALLSWANPLANT.style.width = '100%';
-  ISMALLSWANPLANT.style.height = '100%';
-  ISMALLSWANPLANT.style.margin = '0%';
-  // Medium div
-  /*
- DSMALLSWANPLANT.style.height='73.8%';
-    DMEDSWANPLANT.style.height='73.8%'; /* '73.3%*
-
-    PLANTGRIDBOT.style.marginTop='4.4%';
-/*
-    DLARGESWANPLANT.style.marginTop='5.5%';
-    DEXLARGESWANPLANT.style.marginTop='5.5%';
- /* DMEDSWANPLANT.style.height='73.3%'; */ /* 58.6*
-
-   PLANTGRID.style.gridTemplateRows=' 1fr 0.977fr'
-*/
-console.log("start");
-}
-
-
-function fbp_viewExitSmall(){
-  const PLANTGRIDBOT = document.getElementById('d_viewPlantsGridBot')
-
-const ISMALLSWANPLANT = document.getElementById('i_smallSwanPlant');
-const DSMALLSWANPLANT = document.getElementById('d_smallSwanPlant')
-const DMEDSWANPLANT = document.getElementById('d_mediumSwanPlant')
-
-const DLARGESWANPLANT = document.getElementById('d_largeSwanPlant');
-const DEXLARGESWANPLANT = document.getElementById('d_extraLargeSwanPlant');
-
-
-/**/ 
-ISMALLSWANPLANT.style.width = '90%';
-ISMALLSWANPLANT.style.height = '90%';
-ISMALLSWANPLANT.style.margin = '5%'; 
-
-/*
-DSMALLSWANPLANT.style.height='75%';
-DMEDSWANPLANT.style.height='75%';
-/**
-PLANTGRIDBOT.style.marginTop ='5%';
-/*
-   DLARGESWANPLANT.style.marginTop='7.5%';
-DEXLARGESWANPLANT.style.marginTop='7.5%';
-   /*
-#d_viewPlantsGrid{
-  display:grid;
-  grid-template-columns: repeat(2,1fr);
-  grid-template-rows: repeat(2, 1fr);
-margin-bottom:10%;
-}
-
-   
-   PLANTGRID.style.gridTemplateRows=' repeat(2, 1fr)'
-*/
-console.log("end");
-}
-
-
-
-function fbp_viewEnterMed(){
-  const IMEDSWANPLANT = document.getElementById('i_mediumSwanPlant');
-const DSMALLSWANPLANT = document.getElementById('d_smallSwanPlant')
-const DMEDSWANPLANT = document.getElementById('d_mediumSwanPlant')
-
-
-// MED image 
-  IMEDSWANPLANT.style.width = '100%';
-  IMEDSWANPLANT.style.height = '100%';
-  IMEDSWANPLANT.style.margin = '0%';
-  // Small div
-
-   /*
- DSMALLSWANPLANT.style.height='73.8%';
-    DMEDSWANPLANT.style.height='73.8%'; /* '73.3%*
-    */
-}
-
-
-function fbp_viewExitMed(){
-
-   const IMEDSWANPLANT = document.getElementById('i_mediumSwanPlant');
-const DSMALLSWANPLANT = document.getElementById('d_smallSwanPlant')
-const DMEDSWANPLANT = document.getElementById('d_mediumSwanPlant')
-
-
-  IMEDSWANPLANT.style.width = '90%';
-IMEDSWANPLANT.style.height = '90%';
-IMEDSWANPLANT.style.margin = '5%';
-/*
-DSMALLSWANPLANT.style.height='75%'; 
-DMEDSWANPLANT.style.height='75%';
-*/
-}
-
-
-
-function fbp_viewEnterLarge(){
-const ILARGESWANPLANT = document.getElementById('i_largeSwanPlant');
-
-const DLARGESWANPLANT =  document.getElementById('d_largeSwanPlant');
-
-const DEXLARGESWANPLANT =  document.getElementById('d_extraLargeSwanPlant');
-
-  // MED image 
-  ILARGESWANPLANT.style.width = '100%';
-  ILARGESWANPLANT.style.height = '100%';
-  ILARGESWANPLANT.style.margin = '0%';
-  // Small div
-/*
-  DLARGESWANPLANT.style.height='74%'; 
-DEXLARGESWANPLANT.style.height='74%';
-console.log("hihih")
-*/
-}
-
-function fbp_viewExitLarge(){
-  const ILARGESWANPLANT = document.getElementById('i_largeSwanPlant');
-const IEXLARGESWANPLANT = document.getElementById('i_extraLargeSwanPlant');
-const DLARGESWANPLANT =  document.getElementById('d_largeSwanPlant');
-
-const DEXLARGESWANPLANT =  document.getElementById('d_extraLargeSwanPlant');
-
-  ILARGESWANPLANT.style.width = '90%';
-ILARGESWANPLANT.style.height = '90%';
-ILARGESWANPLANT.style.margin = '5%';
-
-/*
-DLARGESWANPLANT.style.height='75%'; 
-DEXLARGESWANPLANT.style.height='75%';
-console.log("ihihipio")
-*/
-}
-
-function fbp_viewEnterExLarge(){
-const IEXLARGESWANPLANT = document.getElementById('i_extraLargeSwanPlant');
-
-const DLARGESWANPLANT =  document.getElementById('d_largeSwanPlant');
-
-const DEXLARGESWANPLANT =  document.getElementById('d_extraLargeSwanPlant');
-
-  // MED image 
-  IEXLARGESWANPLANT.style.width = '100%';
-  IEXLARGESWANPLANT.style.height = '100%';
-  IEXLARGESWANPLANT.style.margin = '0%';
-  // Small div
-
-}
-
-
-function fbp_viewExitExLarge(){
-  const ILARGESWANPLANT = document.getElementById('i_largeSwanPlant');
-const IEXLARGESWANPLANT = document.getElementById('i_extraLargeSwanPlant');
-
-const DLARGESWANPLANT =  document.getElementById('d_largeSwanPlant');
-
-const DEXLARGESWANPLANT =  document.getElementById('d_extraLargeSwanPlant');
-
-  IEXLARGESWANPLANT.style.width = '90%';
-IEXLARGESWANPLANT.style.height = '90%';
-IEXLARGESWANPLANT.style.margin = '5%';
-
-/*
-DLARGESWANPLANT.style.height='75%'; 
-DEXLARGESWANPLANT.style.height='75%';
-*/
-}
-
-
